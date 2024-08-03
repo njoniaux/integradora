e0270a84a3e86093144bb4f6c24f47bd2430e865
njoniaux <70422868+njoniaux@users.noreply.github.com>
2024-08-02 22:30:41 -0500

    Update auth.py

--- backend/app/api/routers/auth.py
+++ backend/app/api/routers/auth.py
@@ -10,7 +10,7 @@ import bcrypt
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+SECRET_KEY = "asdasd123123"
 ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 
